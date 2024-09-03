Fix masking of data in `_group_average()` - This method now masks data using the weights grouped properly, instead of using the `weight_var_with_weight_threshold()` function - Add `from __future__ import annotations` to `spatial.py` - Add and update units

tests/test_temporal.py
@@ -489,14 +489,6 @@ class TestGroupAverage:
                             cftime.DatetimeGregorian(2001, 1, 1),
                         ],
                     ),
-                    coords={
-                        "time": np.array(
-                            [
-                                cftime.DatetimeGregorian(2000, 1, 1),
-                                cftime.DatetimeGregorian(2001, 1, 1),
-                            ],
-                        )
-                    },
                     dims=["time"],
                     attrs={
                         "axis": "T",
@@ -540,14 +532,6 @@ class TestGroupAverage:
                             cftime.DatetimeGregorian(2001, 1, 1),
                         ],
                     ),
-                    coords={
-                        "time": np.array(
-                            [
-                                cftime.DatetimeGregorian(2000, 1, 1),
-                                cftime.DatetimeGregorian(2001, 1, 1),
-                            ],
-                        )
-                    },
                     dims=["time"],
                     attrs={
                         "axis": "T",
@@ -570,6 +554,195 @@ class TestGroupAverage:
         xr.testing.assert_allclose(result, expected)
         assert result.ts.attrs == expected.ts.attrs
         assert result.time.attrs == expected.time.attrs
+
+    def test_weighted_annual_averages_with_masked_data_and_min_weight_threshold_of_100_percent(
+        self,
+    ):
+        # Set up dataset
+        ds = xr.Dataset(
+            coords={
+                "lat": [-90],
+                "lon": [0],
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            "2000-01-01T00:00:00.000000000",
+                            "2000-02-01T00:00:00.000000000",
+                            "2001-01-01T00:00:00.000000000",
+                            "2001-02-01T00:00:00.000000000",
+                            "2002-01-01T00:00:00.000000000",
+                        ],
+                        dtype="datetime64[ns]",
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            }
+        )
+        ds.time.encoding = {"calendar": "standard"}
+
+        ds["time_bnds"] = xr.DataArray(
+            name="time_bnds",
+            data=np.array(
+                [
+                    ["2000-01-01T00:00:00.000000000", "2000-02-01T00:00:00.000000000"],
+                    ["2000-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2001-01-01T00:00:00.000000000", "2000-01-01T00:00:00.000000000"],
+                    ["2001-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2002-01-01T00:00:00.000000000", "2002-02-01T00:00:00.000000000"],
+                ],
+                dtype="datetime64[ns]",
+            ),
+            coords={"time": ds.time},
+            dims=["time", "bnds"],
+            attrs={"xcdat_bounds": "True"},
+        )
+
+        ds["ts"] = xr.DataArray(
+            data=np.array([[[2]], [[np.nan]], [[1]], [[1]], [[0.5]]]),
+            coords={"lat": ds.lat, "lon": ds.lon, "time": ds.time},
+            dims=["time", "lat", "lon"],
+        )
+
+        # NOTE: If a cell has a missing value for any of the years, the average
+        # for that year should be masked with a min_weight threshold of 100%.
+        result = ds.temporal.group_average("ts", "year", min_weight=1.0)
+        expected = ds.copy()
+        expected = expected.drop_dims("time")
+        expected["ts"] = xr.DataArray(
+            name="ts",
+            data=np.array([[[np.nan]], [[1]], [[0.5]]]),
+            coords={
+                "lat": expected.lat,
+                "lon": expected.lon,
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            cftime.DatetimeGregorian(2000, 1, 1),
+                            cftime.DatetimeGregorian(2001, 1, 1),
+                            cftime.DatetimeGregorian(2002, 1, 1),
+                        ],
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            },
+            dims=["time", "lat", "lon"],
+            attrs={
+                "test_attr": "test",
+                "operation": "temporal_avg",
+                "mode": "group_average",
+                "freq": "year",
+                "weighted": "True",
+            },
+        )
+
+        xr.testing.assert_allclose(result, expected)
+
+    def test_weighted_annual_averages_with_masked_data_and_min_weight_threshold_of_50_percent(
+        self,
+    ):
+        # Set up dataset
+        ds = xr.Dataset(
+            coords={
+                "lat": [-90],
+                "lon": [0],
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            "2000-01-01T00:00:00.000000000",
+                            "2000-02-01T00:00:00.000000000",
+                            "2001-01-01T00:00:00.000000000",
+                            "2001-02-01T00:00:00.000000000",
+                            "2002-01-01T00:00:00.000000000",
+                        ],
+                        dtype="datetime64[ns]",
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            }
+        )
+        ds.time.encoding = {"calendar": "standard"}
+
+        ds["time_bnds"] = xr.DataArray(
+            name="time_bnds",
+            data=np.array(
+                [
+                    ["2000-01-01T00:00:00.000000000", "2000-02-01T00:00:00.000000000"],
+                    ["2000-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2001-01-01T00:00:00.000000000", "2000-01-01T00:00:00.000000000"],
+                    ["2001-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2002-01-01T00:00:00.000000000", "2002-02-01T00:00:00.000000000"],
+                ],
+                dtype="datetime64[ns]",
+            ),
+            coords={"time": ds.time},
+            dims=["time", "bnds"],
+            attrs={"xcdat_bounds": "True"},
+        )
+
+        ds["ts"] = xr.DataArray(
+            data=np.array([[[2]], [[np.nan]], [[1]], [[1]], [[0.5]]]),
+            coords={"lat": ds.lat, "lon": ds.lon, "time": ds.time},
+            dims=["time", "lat", "lon"],
+        )
+
+        # NOTE: The second cell of "ts" has missing data, but the first cell
+        # has more weight (due to more days in the month of Jan vs. Feb) so the
+        # average for the year is not masked with a min_weight threshold of 50%.
+        result = ds.temporal.group_average("ts", "year", min_weight=0.50)
+        expected = ds.copy()
+        expected = expected.drop_dims("time")
+        expected["ts"] = xr.DataArray(
+            name="ts",
+            data=np.array([[[2.0]], [[1]], [[0.5]]]),
+            coords={
+                "lat": expected.lat,
+                "lon": expected.lon,
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            cftime.DatetimeGregorian(2000, 1, 1),
+                            cftime.DatetimeGregorian(2001, 1, 1),
+                            cftime.DatetimeGregorian(2002, 1, 1),
+                        ],
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            },
+            dims=["time", "lat", "lon"],
+            attrs={
+                "test_attr": "test",
+                "operation": "temporal_avg",
+                "mode": "group_average",
+                "freq": "year",
+                "weighted": "True",
+            },
+        )
+
+        xr.testing.assert_allclose(result, expected)
 
     def test_weighted_seasonal_averages_with_DJF_and_drop_incomplete_seasons(self):
         ds = self.ds.copy()
@@ -625,6 +798,14 @@ class TestGroupAverage:
         self,
     ):
         ds = self.ds.copy()
+        ds["ts"] = xr.DataArray(
+            data=np.array(
+                [[[2.0]], [[1.0]], [[1.0]], [[1.0]], [[2.0]]], dtype="float64"
+            ),
+            coords={"time": self.ds.time, "lat": self.ds.lat, "lon": self.ds.lon},
+            dims=["time", "lat", "lon"],
+            attrs={"test_attr": "test"},
+        )
 
         result = ds.temporal.group_average(
             "ts",
@@ -698,17 +879,102 @@ class TestGroupAverage:
                             cftime.DatetimeGregorian(2001, 1, 1),
                         ],
                     ),
-                    coords={
-                        "time": np.array(
-                            [
-                                cftime.DatetimeGregorian(2000, 1, 1),
-                                cftime.DatetimeGregorian(2000, 4, 1),
-                                cftime.DatetimeGregorian(2000, 7, 1),
-                                cftime.DatetimeGregorian(2000, 10, 1),
-                                cftime.DatetimeGregorian(2001, 1, 1),
-                            ],
-                        )
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
                     },
+                ),
+            },
+            dims=["time", "lat", "lon"],
+            attrs={
+                "test_attr": "test",
+                "operation": "temporal_avg",
+                "mode": "group_average",
+                "freq": "season",
+                "weighted": "True",
+                "dec_mode": "JFD",
+            },
+        )
+
+        xr.testing.assert_identical(result, expected)
+
+    def test_weighted_seasonal_averages_with_JFD_with_min_weight_threshold_of_100_percent(
+        self,
+    ):
+        time = xr.DataArray(
+            data=np.array(
+                [
+                    "2000-01-16T12:00:00.000000000",
+                    "2000-02-15T12:00:00.000000000",
+                    "2000-03-16T12:00:00.000000000",
+                    "2000-06-16T00:00:00.000000000",
+                    "2000-12-16T00:00:00.000000000",
+                ],
+                dtype="datetime64[ns]",
+            ),
+            dims=["time"],
+            attrs={"axis": "T", "long_name": "time", "standard_name": "time"},
+        )
+        time.encoding = {"calendar": "standard"}
+        time_bnds = xr.DataArray(
+            name="time_bnds",
+            data=np.array(
+                [
+                    ["2000-01-01T00:00:00.000000000", "2000-02-01T00:00:00.000000000"],
+                    ["2000-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2000-03-01T00:00:00.000000000", "2000-04-01T00:00:00.000000000"],
+                    ["2000-06-01T00:00:00.000000000", "2000-07-01T00:00:00.000000000"],
+                    ["2000-11-01T00:00:00.000000000", "2001-01-01T00:00:00.000000000"],
+                ],
+                dtype="datetime64[ns]",
+            ),
+            coords={"time": time},
+            dims=["time", "bnds"],
+            attrs={"xcdat_bounds": "True"},
+        )
+
+        ds = xr.Dataset(
+            data_vars={"time_bnds": time_bnds},
+            coords={"lat": [-90], "lon": [0], "time": time},
+        )
+        ds.time.attrs["bounds"] = "time_bnds"
+
+        ds["ts"] = xr.DataArray(
+            data=np.array(
+                [[[np.nan]], [[1.0]], [[1.0]], [[1.0]], [[2.0]]], dtype="float64"
+            ),
+            coords={"time": self.ds.time, "lat": self.ds.lat, "lon": self.ds.lon},
+            dims=["time", "lat", "lon"],
+            attrs={"test_attr": "test"},
+        )
+
+        # NOTE: If a cell has a missing value for any of the seasons, the average
+        # for that season should be masked with a min_weight threshold of 100%.
+        result = ds.temporal.group_average(
+            "ts",
+            "season",
+            season_config={"dec_mode": "JFD"},
+            min_weight=1.0,
+        )
+        expected = ds.copy()
+        expected = expected.drop_dims("time")
+        expected["ts"] = xr.DataArray(
+            name="ts",
+            data=np.array([[[np.nan]], [[1.0]], [[1.0]]]),
+            coords={
+                "lat": expected.lat,
+                "lon": expected.lon,
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            cftime.DatetimeGregorian(2000, 1, 1),
+                            cftime.DatetimeGregorian(2000, 4, 1),
+                            cftime.DatetimeGregorian(2000, 7, 1),
+                        ],
+                    ),
                     dims=["time"],
                     attrs={
                         "axis": "T",
@@ -902,6 +1168,102 @@ class TestGroupAverage:
                             cftime.DatetimeGregorian(2000, 3, 1),
                             cftime.DatetimeGregorian(2000, 6, 1),
                             cftime.DatetimeGregorian(2000, 9, 1),
+                            cftime.DatetimeGregorian(2001, 2, 1),
+                        ],
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            },
+            dims=["time", "lat", "lon"],
+            attrs={
+                "test_attr": "test",
+                "operation": "temporal_avg",
+                "mode": "group_average",
+                "freq": "month",
+                "weighted": "True",
+            },
+        )
+
+        xr.testing.assert_identical(result, expected)
+
+    def test_weighted_monthly_averages_with_masked_data_and_min_weight_threshold_of_100_percent(
+        self,
+    ):
+        # Set up dataset
+        ds = xr.Dataset(
+            coords={
+                "lat": [-90],
+                "lon": [0],
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            "2000-01-01T00:00:00.000000000",
+                            "2000-02-01T00:00:00.000000000",
+                            "2000-02-15T00:00:00.000000000",
+                            "2000-04-01T00:00:00.000000000",
+                            "2001-02-01T00:00:00.000000000",
+                        ],
+                        dtype="datetime64[ns]",
+                    ),
+                    dims=["time"],
+                    attrs={
+                        "axis": "T",
+                        "long_name": "time",
+                        "standard_name": "time",
+                        "bounds": "time_bnds",
+                    },
+                ),
+            }
+        )
+        ds.time.encoding = {"calendar": "standard"}
+
+        ds["time_bnds"] = xr.DataArray(
+            name="time_bnds",
+            data=np.array(
+                [
+                    ["2000-01-01T00:00:00.000000000", "2000-02-01T00:00:00.000000000"],
+                    ["2000-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2000-02-01T00:00:00.000000000", "2000-03-01T00:00:00.000000000"],
+                    ["2000-04-01T00:00:00.000000000", "2000-05-01T00:00:00.000000000"],
+                    ["2001-02-01T00:00:00.000000000", "2001-03-01T00:00:00.000000000"],
+                ],
+                dtype="datetime64[ns]",
+            ),
+            coords={"time": ds.time},
+            dims=["time", "bnds"],
+            attrs={"xcdat_bounds": "True"},
+        )
+
+        ds["ts"] = xr.DataArray(
+            data=np.array([[[2]], [[np.nan]], [[1]], [[1]], [[1]]]),
+            coords={"lat": ds.lat, "lon": ds.lon, "time": ds.time},
+            dims=["time", "lat", "lon"],
+            attrs={"test_attr": "test"},
+        )
+
+        # NOTE: If a cell has a missing value for any of the months, the average
+        # for that month should be masked with a min_weight threshold of 100%.
+        result = ds.temporal.group_average("ts", "month", min_weight=0.55)
+        expected = ds.copy()
+        expected = expected.drop_dims("time")
+        expected["ts"] = xr.DataArray(
+            name="ts",
+            data=np.array([[[2.0]], [[np.nan]], [[1.0]], [[1.0]]]),
+            coords={
+                "lat": expected.lat,
+                "lon": expected.lon,
+                "time": xr.DataArray(
+                    data=np.array(
+                        [
+                            cftime.DatetimeGregorian(2000, 1, 1),
+                            cftime.DatetimeGregorian(2000, 2, 1),
+                            cftime.DatetimeGregorian(2000, 4, 1),
                             cftime.DatetimeGregorian(2001, 2, 1),
                         ],
                     ),

xcdat/temporal.py
@@ -19,11 +19,7 @@ from xcdat import bounds  # noqa: F401
 from xcdat._logger import _setup_custom_logger
 from xcdat.axis import get_dim_coords
 from xcdat.dataset import _get_data_var
-from xcdat.utils import (
-    _get_masked_weights,
-    _validate_min_weight,
-    mask_var_with_weight_threshold,
-)
+from xcdat.utils import _get_masked_weights, _validate_min_weight
 
 logger = _setup_custom_logger(__name__)
 
@@ -166,7 +162,6 @@ class TemporalAccessor:
         data_var: str,
         weighted: bool = True,
         keep_weights: bool = False,
-        min_weight: float | None = None,
     ):
         """
         Returns a Dataset with the average of a data variable and the time
@@ -208,10 +203,6 @@ class TemporalAccessor:
         keep_weights : bool, optional
             If calculating averages using weights, keep the weights in the
             final dataset output, by default False.
-        min_weight : float | None, optional
-            Fraction of data coverage (i..e, weight) needed to return a
-            spatial average value. Value must range from 0 to 1, by default None
-            (equivalent to ``min_weight=0.0``).
 
         Returns
         -------
@@ -252,7 +243,6 @@ class TemporalAccessor:
             freq,
             weighted=weighted,
             keep_weights=keep_weights,
-            min_weight=min_weight,
         )
 
     def group_average(
@@ -262,6 +252,7 @@ class TemporalAccessor:
         weighted: bool = True,
         keep_weights: bool = False,
         season_config: SeasonConfigInput = DEFAULT_SEASON_CONFIG,
+        min_weight: float | None = None,
     ):
         """Returns a Dataset with average of a data variable by time group.
 
@@ -340,6 +331,10 @@ class TemporalAccessor:
                 >>>     ["Jul", "Aug", "Sep"],  # "JulAugSep"
                 >>>     ["Oct", "Nov", "Dec"],  # "OctNovDec"
                 >>> ]
+        min_weight : float | None, optional
+            Fraction of data coverage (i..e, weight) needed to return a
+            temporal average value. Value must range from 0 to 1, by default
+            None ((equivalent to ``min_weight=0.0``).
 
         Returns
         -------
@@ -418,6 +413,7 @@ class TemporalAccessor:
             weighted=weighted,
             keep_weights=keep_weights,
             season_config=season_config,
+            min_weight=min_weight,
         )
 
     def climatology(
@@ -911,31 +907,36 @@ class TemporalAccessor:
     ):
         """Validates method arguments and sets them as object attributes.
 
-        Parameters
-        ----------
-        mode : Mode
-            The mode for temporal averaging.
-        freq : Frequency
-            The frequency of time to group by.
-        weighted : bool
-            Calculate averages using weights.
-        season_config: Optional[SeasonConfigInput]
-            A dictionary for "season" frequency configurations. If configs for
-            predefined seasons are passed, configs for custom seasons are
-            ignored and vice versa, by default DEFAULT_SEASON_CONFIG.
-        min_weight : float | None, optional
-            Fraction of data coverage (i..e, weight) needed to return a
-            spatial average value. Value must range from 0 to 1, by default None
-            (equivalent to ``min_weight=0.0``).
+                Parameters
+                ----------
+                mode : Mode
+                    The mode for temporal averaging.
+                freq : Frequency
+                    The frequency of time to group by.
+                weighted : bool
+                    Calculate averages using weights.
+                season_config: Optional[SeasonConfigInput]
+                    A dictionary for "season" frequency configurations. If configs for
+                    predefined seasons are passed, configs for custom seasons are
+                    ignored and vice versa, by default DEFAULT_SEASON_CONFIG.
+                min_weight : float | None, optional
+                    Fraction of data coverage (i..e, weight) needed to return a
+        <<<<<<< Updated upstream
+                    spatial average value. Value must range from 0 to 1, by default None
+                    (equivalent to ``min_weight=0.0``).
+        =======
+                    temporal average value. Value must range from 0 to 1, by default
+                    None ((equivalent to ``min_weight=0.0``).
+        >>>>>>> Stashed changes
 
-        Raises
-        ------
-        KeyError
-            If the Dataset does not have a time dimension.
-        ValueError
-            If an incorrect ``freq`` arg was passed.
-        ValueError
-            If an incorrect ``dec_mode`` arg was passed.
+                Raises
+                ------
+                KeyError
+                    If the Dataset does not have a time dimension.
+                ValueError
+                    If an incorrect ``freq`` arg was passed.
+                ValueError
+                    If an incorrect ``dec_mode`` arg was passed.
         """
         # General configuration attributes.
         if mode not in list(MODES):
@@ -1172,11 +1173,6 @@ class TemporalAccessor:
                 self._weights = self._get_weights(time_bounds)
 
                 dv = dv.weighted(self._weights).mean(dim=self.dim)
-
-                if self._min_weight > 0.0:
-                    dv = mask_var_with_weight_threshold(
-                        dv, self._weights, self._min_weight
-                    )
             else:
                 dv = dv.mean(dim=self.dim)
 
@@ -1210,34 +1206,44 @@ class TemporalAccessor:
             time_bounds = ds.bounds.get_bounds("T", var_key=data_var)
             self._weights = self._get_weights(time_bounds)
 
-            # Weight the data variable.
-            dv *= self._weights
-
-            # Perform weighted average using the formula
-            # WA = sum(data*weights) / sum(masked weights). The denominator must
-            # be included to take into account zero weight for missing data.
-            masked_weights = _get_masked_weights(dv, self._weights)
             with xr.set_options(keep_attrs=True):
-                dv = self._group_data(dv).sum() / self._group_data(masked_weights).sum()
+                dv_weighted = dv * self._weights
 
+                # Perform weighted average using the formula
+                # # WA = sum(data*weights) / sum(masked weights).
+                # The denominator must be included to take into account zero
+                # weight for missing data.
+                dv_group_sum = self._group_data(dv_weighted).sum()
+                weights_masked = _get_masked_weights(dv_weighted, self._weights)
+                weights_masked_group_sum = self._group_data(weights_masked).sum()
+
+                dv_avg = dv_group_sum / weights_masked_group_sum
+
+            # Mask the data variable values with weights below the minimum
+            # weight threshold (if specified).
             if self._min_weight > 0.0:
-                dv = mask_var_with_weight_threshold(dv, self._weights, self._min_weight)
+                dv_avg = xr.where(
+                    weights_masked_group_sum >= self._min_weight,
+                    dv_avg,
+                    np.nan,
+                    keep_attrs=True,
+                )
 
             # Restore the data variable's name.
-            dv.name = data_var
+            dv_avg.name = data_var
         else:
-            dv = self._group_data(dv).mean()
+            dv_avg = self._group_data(dv).mean()
 
         # After grouping and aggregating, the grouped time dimension's
         # attributes are removed. Xarray's `keep_attrs=True` option only keeps
         # attributes for data variables and not their coordinates, so the
         # coordinate attributes have to be restored manually.
-        dv[self.dim].attrs = self._labeled_time.attrs
-        dv[self.dim].encoding = self._labeled_time.encoding
+        dv_avg[self.dim].attrs = self._labeled_time.attrs
+        dv_avg[self.dim].encoding = self._labeled_time.encoding
 
-        dv = self._add_operation_attrs(dv)
+        dv_avg = self._add_operation_attrs(dv_avg)
 
-        return dv
+        return dv_avg
 
     def _get_weights(self, time_bounds: xr.DataArray) -> xr.DataArray:
         """Calculates weights for a data variable using time bounds.

xcdat/utils.py
@@ -174,7 +174,8 @@ def mask_var_with_weight_threshold(
     frac = weight_sum_masked / weight_sum_all
 
     # Nan out values that don't meet specified weight threshold.
-    dv_new = xr.where(frac >= min_weight, dv, np.nan)
+    dv_new = xr.where(frac >= min_weight, dv, np.nan, keep_attrs=True)
+    dv_new.name = dv.name
 
     return dv_new
 
@@ -196,8 +197,7 @@ def _get_masked_weights(dv: xr.DataArray, weights: xr.DataArray) -> xr.DataArray
     xr.DataArray
         The masked weights.
     """
-    masked_weights, _ = xr.broadcast(weights, dv)
-    masked_weights = xr.where(dv.copy().isnull(), 0.0, masked_weights)
+    masked_weights = xr.where(dv.copy().isnull(), 0.0, weights)
 
     return masked_weights
 